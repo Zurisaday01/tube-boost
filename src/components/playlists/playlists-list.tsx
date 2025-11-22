f559c446c890fc17b8e631260da56a23a1db086e
Zury01 <Zurisaday_01@hotmail.com>
2025-11-22 09:31:25 -0600

    feat: reusable playlist list and modifications for mobile

---
+++ src/components/playlists/playlists-list.tsx
@@ -0,0 +1,26 @@
+import { PlaylistWithStats } from '@/types/actions';
+import PlaylistCard from './playlist-card';
+
+interface PlaylistsListProps {
+  playlists: PlaylistWithStats[] | undefined;
+}
+
+const PlaylistsList = ({ playlists }: PlaylistsListProps) => {
+  return (
+    <div className='w-full grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+      {playlists?.map((playlist) => (
+        <PlaylistCard
+          key={playlist.id}
+          id={playlist.id}
+          title={playlist.title}
+          playlistType={playlist.playlistType}
+          createdAt={playlist.createdAt}
+          updatedAt={playlist.updatedAt}
+          totalVideos={playlist.totalVideos}
+          totalCategories={playlist.totalCategories}
+        />
+      ))}
+    </div>
+  );
+};
+export default PlaylistsList;

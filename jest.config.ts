88814b572f7ac864bb254ca78ea00b14ec6980b2
Zury01 <Zurisaday_01@hotmail.com>
2025-11-30 13:49:06 -0600

    fix: move Jest options out of nextJest() and into customJestConfig

--- jest.config.ts
+++ jest.config.ts
@@ -1,29 +1,16 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
-
-  // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
-
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8'
+  // Path to load next.config.js and .env files
+  dir: './'
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   testEnvironmentOptions: {
